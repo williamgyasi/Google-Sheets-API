b2e468b0f48925df71ee279367c36925e74024db
William Gyasi <kwabenagyasi63@gmail.com>
2020-09-11 12:03:56 +0000

    Index Path

--- functions/index.js
+++ functions/index.js
@@ -1,9 +1,20 @@
 const functions = require('firebase-functions');
+const firebase=require('firebase-admin')
+const express=require('express');
+const engines=require('consolidate')
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+const firebaseApp =firebase.initializeApp(
+    functions.config().firebase
+)
+
+
+
+const app=express()
+app.engine('pug',engines.handlebars)
+app.set('views','./views')
+app.set('view engine','pug')
+exports.app = functions.https.onRequest(app)
+
+app.get('/',(request,response)=>{
+    response.render('index.pug')
+})
