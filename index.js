6ce6bc1ab3f0b4ea9899ebe165a69d142ff2fbfb
William Gyasi <kwabenagyasi63@gmail.com>
2020-09-12 20:50:56 +0000

    Heroku Config

--- index.js
+++ index.js
@@ -95,7 +95,7 @@ app.post('/downloadsheet',async (req,res)=>{
 })
 
 
-const PORT = process.env.PORT || 8000;
+const PORT = process.env.PORT || 8080;
 const HOST='0.0.0.0'
 
 app.listen(PORT, () => {
